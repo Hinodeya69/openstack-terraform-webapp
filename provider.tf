422dc5ca4be0fce3e7786dc386738833a31b6446
eazytrainingfr <eazytrainingfr@gmail.com>
2020-10-15 12:17:56 +0000

    first comment

---
+++ provider.tf
@@ -0,0 +1,13 @@
+# Configure the OpenStack Provider
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "1.32.0"
+    }
+  }
+}
+
+provider "openstack" {
+}
+
